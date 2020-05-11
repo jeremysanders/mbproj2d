bbc825884d9a30dab976fc6c810d0c90343e186f
Jeremy Sanders <jeremy@jeremysanders.net>
2020-05-11 08:57:52 +0200

    Rename cython file

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 fastsourcefiles = [
-    'mbproj2d/fast/project.pyx',
+    'mbproj2d/fast/fast.pyx',
     'mbproj2d/fast/project_cc.cc',
     'mbproj2d/fast/binimg_cc.cc',
 ]
